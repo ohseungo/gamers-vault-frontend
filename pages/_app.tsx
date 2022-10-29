8759c456f9ca46eb7ae09dced4255cf620af4f0b
OH SW <ohseungo@naver.com>
2022-10-29 21:57:40 +0900

    feat : tailwind 설정 및 Navbar css 기본

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Layout from "components/common/Layout";
 import type { AppProps } from "next/app";
 
 import "styles/main.css";
+import "styles/base.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
