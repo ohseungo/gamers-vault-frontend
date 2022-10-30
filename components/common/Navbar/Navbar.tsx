7f27875a9d3787f64ce327ca15208f02a0a9e388
OH SW <ohseungo@naver.com>
2022-10-30 15:24:30 +0900

    feat : Searchbar 추가

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,11 +1,15 @@
+import Searchbar from "../Searchbar";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.root}>
       <div className={styles.nav}>
-        <div>Logo</div>
-        <div>Navbar here</div>
+        <div className="flex-1">Logo</div>
+        <div className="flex-1">
+          <Searchbar />
+        </div>
+        <div className="flex-1">Navbar</div>
       </div>
     </div>
   );
--- components/common/Searchbar/Searchbar.tsx
+++ components/common/Searchbar/Searchbar.tsx
@@ -1,3 +1,19 @@
-const Searchbar = () => {};
+import styles from "./Searchbar.module.css";
+const Searchbar = () => {
+  return (
+    <div className={styles.root}>
+      <input className={styles.input}></input>
+      <div className={styles.iconContainer}>
+        <svg className={styles.icon} fill="currentColor" viewBox="0 0 20 20">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export default Searchbar;
