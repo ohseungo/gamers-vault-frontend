e020d4a46416f0b8b16a149598eefe60b1cce700
OH SW <ohseungo@naver.com>
2022-10-31 21:42:37 +0900

    feat : login view

---
+++ components/auth/LoginView/LoginView.tsx
@@ -0,0 +1,11 @@
+import style from "./LoginView.module.css";
+
+const LoginView = () => {
+  return (
+    <div>
+      <div>LoginView</div>
+    </div>
+  );
+};
+
+export default LoginView;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import LoginView from "components/auth/LoginView";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="flex flex-row">
+      <LoginView />
+    </div>
+  );
 };
 
 export async function getStaticProps() {
