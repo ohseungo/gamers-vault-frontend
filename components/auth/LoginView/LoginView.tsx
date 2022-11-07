98e5635aee23634fae289b7a078f7c78713edd69
OH SW <ohseungo@naver.com>
2022-11-07 18:41:48 +0900

    공통 ui component - button

--- components/auth/LoginView/LoginView.tsx
+++ components/auth/LoginView/LoginView.tsx
@@ -1,3 +1,4 @@
+import Button from "components/ui/Button";
 import Input from "components/ui/Input";
 import style from "./LoginView.module.css";
 
@@ -7,6 +8,7 @@ const LoginView = () => {
       <h1>Gamers Vault</h1>
       <Input type="email" placeholder="이메일" />
       <Input type="password" placeholder="비밀번호" className="mt-2" />
+      <Button>로그인</Button>
     </div>
   );
 };
--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,10 +1,20 @@
 import { ButtonHTMLAttributes } from "react";
 import style from "./Button.module.css";
+import cn from "classnames";
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  disabled?: boolean;
+}
 
-const Button = ({ ...rest }: Props) => {
-  return <button>Hello</button>;
+const Button = ({ className, disabled, children, ...rest }: Props) => {
+  const rootClassName = cn(style.root, {}, className);
+
+  return (
+    <button className={rootClassName} disabled={disabled}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
