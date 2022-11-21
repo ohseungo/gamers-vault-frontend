282b165d4b1b76691d3f09516fcc44ddfd7e8581
OH SW <ohseungo@naver.com>
2022-11-21 21:47:03 +0900

    feat : redux-devtools 설치

--- store/reducers/index.ts
+++ store/reducers/index.ts
@@ -1,4 +1,15 @@
 import { combineReducers } from "@reduxjs/toolkit";
-const rootReducer = combineReducers({});
+import userReducer from "./userReducer";
+import HYDRATE from "next-redux-wrapper";
+const rootReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case HYDRATE:
+      return action.payload;
+    default:
+      return combineReducers({
+        user: userReducer,
+      })(state, action);
+  }
+};
 
 export default rootReducer;
