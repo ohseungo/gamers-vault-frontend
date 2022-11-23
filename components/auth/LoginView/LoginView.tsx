a6725b26358b9629aee3f3bd89d2971398fd17c0
OH SW <ohseungo@naver.com>
2022-11-23 22:08:13 +0900

    feat : react-redux 설치 및 provider wrapper

--- components/auth/LoginView/LoginView.tsx
+++ components/auth/LoginView/LoginView.tsx
@@ -1,12 +1,17 @@
 import Button from "components/ui/Button";
 import Input from "components/ui/Input";
+import React from "react";
 import style from "./LoginView.module.css";
 
 const LoginView = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("!");
+  };
   return (
     <div className={style.root}>
       <h1>Gamers Vault</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col space-y-2 w-full items-center">
           <Input type="email" placeholder="이메일" />
           <Input type="password" placeholder="비밀번호" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
 import Head from "components/common/Head";
 import Layout from "components/common/Layout";
 import type { AppProps } from "next/app";
-
+import { Provider } from "react-redux";
 import "styles/main.css";
 import "styles/base.css";
 import wrapper from "store/store";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
   return (
-    <>
+    <Provider store={store}>
       <Head />
-      <Layout pageProps={pageProps}>
-        <Component {...pageProps} />
+      <Layout pageProps={props.pageProps}>
+        <Component {...props.pageProps} />
       </Layout>
-    </>
+    </Provider>
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
