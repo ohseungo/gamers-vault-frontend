43429abddde7ade56a9ee008a60169c727b772ed
OH SW <ohseungo@naver.com>
2022-11-23 21:48:00 +0900

    feat : login form

--- components/auth/LoginView/LoginView.tsx
+++ components/auth/LoginView/LoginView.tsx
@@ -6,11 +6,13 @@ const LoginView = () => {
   return (
     <div className={style.root}>
       <h1>Gamers Vault</h1>
-      <div className="flex flex-col space-y-2 w-full items-center">
-        <Input type="email" placeholder="이메일" />
-        <Input type="password" placeholder="비밀번호" />
-        <Button>로그인</Button>
-      </div>
+      <form>
+        <div className="flex flex-col space-y-2 w-full items-center">
+          <Input type="email" placeholder="이메일" />
+          <Input type="password" placeholder="비밀번호" />
+          <Button type="submit">로그인</Button>
+        </div>
+      </form>
     </div>
   );
 };
