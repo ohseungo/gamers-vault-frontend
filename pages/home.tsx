c6a8eb2cbd3857741f965a49e6f4d6dc358295ed
OH SW <ohseungo@naver.com>
2022-11-27 21:05:11 +0900

    feat: login user redux

---
+++ pages/home.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col justify-center h-full">
+      <div className="flex flex-row justify-center">Home Page</div>
+    </div>
+  );
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import LoginView from "components/auth/LoginView";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { RootState } from "store/reducers";
 
 const Home: NextPage = () => {
+  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+  const router = useRouter();
+
+  if (isLoggedIn) router.replace("/home");
+
   return (
     <div className="flex flex-col justify-center h-full">
       <div className="flex flex-row justify-center">
