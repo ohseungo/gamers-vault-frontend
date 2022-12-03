2ee0a2446d549f97ef3be9484141518e86fd868d
OH SW <ohseungo@naver.com>
2022-12-03 22:37:42 +0900

    feat : container 적용 test

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,12 +2,7 @@ import Container from "components/ui/Container/container";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    //   <div className="flex flex-col justify-center h-full">
-    /* <div className="flex flex-row justify-center">Home Page</div> */
-    <Container>Home Page</Container>
-    // </div>
-  );
+  return <Container>Home Page</Container>;
 };
 
 export default Home;
