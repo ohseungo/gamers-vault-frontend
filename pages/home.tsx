58eee1494a710fc28cd3e366f54ab318de4efd64
OH SW <ohseungo@naver.com>
2022-12-06 22:08:53 +0900

    feat : feeds 기본

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,8 +1,14 @@
-import Container from "components/ui/Container/container";
+import FeedList from "components/feed/FeedList/FeedList";
+import Container from "components/ui/Container/Container";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <Container>Home Page</Container>;
+  return (
+    <Container>
+      Home Page
+      <FeedList />
+    </Container>
+  );
 };
 
 export default Home;
